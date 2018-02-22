e172cfdcc6e8586278570ae14256b5ce1a2af15d
Andreas Buttenschoen <andreas.buttenschoen@gmail.com>
2018-02-21 22:50:17 -0700

    Move some commonly used functions to helpers

---
+++ tdr/helpers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: Andreas Buttenschoen
+import numpy as np
+
+
+def getHillFunction(n):
+    hillFunction = lambda x : np.power(x, n) / (1. + np.power(x, n))
+    return hillFunction
+
+
+def getStepFunction(x, step_point = 0.1, hfactor = 2.):
+    return np.piecewise(x, [x < step_point, x >= step_point], [hfactor, 1.])
+
+
