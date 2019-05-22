2abb337bc624e38634f11cd6ad4cc1db9636181a
Andreas Buttenschoen <andreas@buttenschoen.ca>
2019-05-21 17:56:51 -0700

    test: clean-up GridTest

--- test/GridTest.py
+++ test/GridTest.py
@@ -7,7 +7,6 @@ import numpy as np
 
 from tdr.Grid import Grid
 from tdr.Domain import Interval
-from tdr.helpers import asarray
 from utils.utils import array_ptr
 
 
@@ -56,38 +55,26 @@ class GridTest(unittest.TestCase):
                     self.assertTrue(np.abs(b - xf_comp[0]) < 1e-8)
 
 
-#if __name__ == '__main__':
-#
-#    nop = 2
-#
-#    dom = Interval(-1, 1, nop=nop)
-#    print('dom:', dom.bds)
-#
-#    # test interval first
-#    print('dX:', dom.dX, ' shape:', dom.dX.shape, ' dX:', dom.dX[1])
-#    print('x0s:', dom.x0s, ' x0:', dom.x0)
-#    print('xfs:', dom.xfs, ' xf:', dom.xf)
-#    print('N:', dom.N)
-#    print('x:', dom.xs())
-#
-#    ngb = np.asarray(dom.bd).reshape(1)
-#    dX  = asarray(dom.dX)
-#    x0  = asarray(dom.origin())
-#    N   = asarray(dom.N, np.int)
-#
-#    print('dX:', dX)
-#
-#    grid = Grid(1, dom, bw=2)
-#
-#    print('patches:', grid.patches)
-#    #print('centers:', grid.cellCentreMatrix)
-#
-#
-#    print('grid:', array_ptr(grid.ydot))
-#
-#    y = np.arange(0, grid.molsize, 1)
-#    y = np.expand_dims(y, axis=0)
-#    grid.update(0, y)
+    def test_grid_update(self):
+        # test parameters
+        n_eqns = 1
+        a      = -1
+        b      = 1
+        bw     = 2
+
+        dom  = Interval(a, b, nop=1)
+        grid = Grid(n_eqns, dom, bw=bw)
+
+        # get ydot pointer
+        y_ptr = array_ptr(grid.ydot)
+
+        # create some fake data for the update
+        y = np.arange(0, grid.molsize, 1)
+        y = np.expand_dims(y, axis=0)
+        grid.update(0, y)
+
+        # check that y_ptr is as before
+        self.assertEqual(y_ptr, array_ptr(grid.ydot))
 
 
 if __name__ == '__main__':
