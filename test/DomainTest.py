ebad02e7de0df76fb28a463cbebf3ee303af697d
Andreas Buttenschoen <andreas@buttenschoen.ca>
2019-07-10 09:42:14 -0700

    test: Add square unit tests and improve domain testing

--- test/DomainTest.py
+++ test/DomainTest.py
@@ -5,9 +5,10 @@ import unittest
 import numpy as np
 
 from tdr.Domain import Interval
+from tdr.Domain import Square
 
 
-class DomainTest(unittest.TestCase):
+class IntervalTest(unittest.TestCase):
 
     def test_interval_construction(self):
         interval = Interval(a = 0., b = 1.)
@@ -22,6 +23,7 @@ class DomainTest(unittest.TestCase):
         self.assertTrue(isinstance(interval.N, np.ndarray))
         self.assertTrue(isinstance(interval.M, np.int))
         self.assertTrue(isinstance(interval.n, np.int))
+        self.assertEqual(len(interval.N.shape), 1)
 
 
     def test_interval_xs(self):
@@ -48,6 +50,8 @@ class DomainTest(unittest.TestCase):
             self.assertEqual(interval.xf, b)
             self.assertLessEqual(Nex, np.sum(interval.N))
             self.assertEqual(np.sum(interval.N), int(interval.L * interval.cellsPerUnitLength))
+            self.assertEqual(len(interval.N.shape), 1)
+            self.assertEqual(interval.N.size, nop)
 
             # now check that dX is set correctly
             Nactual = np.sum(interval.N)
@@ -55,6 +59,43 @@ class DomainTest(unittest.TestCase):
             hexp = L / Nactual
             for i in range(nop):
                 self.assertAlmostEqual(interval.dX[i], hexp)
+
+
+class SquareTest(unittest.TestCase):
+
+    def test_square_construction(self):
+        square = Square(a = 0., b = 1., c = 0., d = 1.)
+
+        self.assertEqual(square.x0, 0.)
+        self.assertEqual(square.xf, 1.)
+        self.assertTrue(np.all(square.L == np.array([1.,1.])))
+        self.assertEqual(square.n,  8)
+        self.assertEqual(square.cellsPerUnitLength,  2**8)
+        self.assertEqual(square.M,  2**8)
+        self.assertEqual(square.h,  1./2**8)
+        self.assertTrue(isinstance(square.N, np.ndarray))
+        self.assertTrue(isinstance(square.M, np.int))
+        self.assertTrue(isinstance(square.n, np.int))
+        self.assertEqual(len(square.N.shape), 2)
+
+
+    def test_square_xs(self):
+        square = Square(a = 0., b = 1., c = 1., d = 2.)
+        x  = square.x
+        self.assertEqual(np.min(x), 0.)
+        self.assertEqual(np.max(x), 1.)
+
+        y = square.y
+        self.assertEqual(np.min(y), 1.)
+        self.assertEqual(np.max(y), 2.)
+
+        xs = square.xs()
+        self.assertEqual(np.min(xs), 0.)
+        self.assertEqual(np.max(xs), 1.)
+
+        ys = square.ys()
+        self.assertEqual(np.min(ys), 1.)
+        self.assertEqual(np.max(ys), 2.)
 
 
 if __name__ == '__main__':
