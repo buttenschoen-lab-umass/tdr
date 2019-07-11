fd285f910efa3770985fb895f02a810f50b03210
Andreas Buttenschoen <andreas@buttenschoen.ca>
2019-07-11 14:36:25 -0700

    test: improve read profile tool by being able to specfiy profile file from command line.

--- test/readProfile.py
+++ test/readProfile.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: Andreas Buttenschoen
 from __future__ import print_function, division, absolute_import
+import os, argparse
 import pstats
 
 #
@@ -10,7 +11,15 @@ import pstats
 # To run a profile execute: python3 -m cProfile -s cumtime -o tdr2d_vanLeer.out test/tdrProfile2D.py
 #
 
-p = pstats.Stats('tdr2d.out')
-p.sort_stats('cumulative').print_stats(50)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Display profile created with cProfile')
+    parser.add_argument('-f', '--file')
+    parser.add_argument('-n', '--lines', dest='lines', default=50)
+    args = parser.parse_args()
+
+    assert os.path.isfile(args.file), '%s can not be found!' % args.file
+
+    p = pstats.Stats(args.file)
+    p.sort_stats('tottime').print_stats(args.lines)
 
 
